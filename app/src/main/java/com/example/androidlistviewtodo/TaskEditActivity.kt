fd94dac6f0f9621ab404ef6bfa974b03f7224b2f
lucasomahony <93211681+lucasomahony@users.noreply.github.com>
2021-12-06 21:39:50 +0000

    added edittext and button

--- app/src/main/java/com/example/androidlistviewtodo/TaskEditActivity.kt
+++ app/src/main/java/com/example/androidlistviewtodo/TaskEditActivity.kt
@@ -1,11 +1,21 @@
 package com.example.androidlistviewtodo
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.androidlistviewtodo.databinding.ActivityTaskEditBinding
 
 class TaskEditActivity : AppCompatActivity() {
     private lateinit var binding : ActivityTaskEditBinding
+
+    fun onSaveClick(view: View){
+        val intent = Intent()
+        intent.putExtra("task_text", binding.edTask.text.toString())
+        setResult(Activity.RESULT_OK, intent)
+        finish()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
