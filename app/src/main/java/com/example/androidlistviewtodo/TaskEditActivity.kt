bf222133d02fc1af2691324b16c4cf5a9f1f7a55
lucasomahony <93211681+lucasomahony@users.noreply.github.com>
2021-12-06 20:50:09 +0000

    add viewbinding code for taskeditactivity

--- app/src/main/java/com/example/androidlistviewtodo/TaskEditActivity.kt
+++ app/src/main/java/com/example/androidlistviewtodo/TaskEditActivity.kt
@@ -2,10 +2,15 @@ package com.example.androidlistviewtodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.androidlistviewtodo.databinding.ActivityTaskEditBinding
 
 class TaskEditActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityTaskEditBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_task_edit)
+        binding = ActivityTaskEditBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
